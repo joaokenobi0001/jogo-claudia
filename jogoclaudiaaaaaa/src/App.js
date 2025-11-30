95cbebeeb601eb1b6477e1d41b9a30439044d51f
João Eduardo Matos Candelmo Do Amaral <115738393+joaokenobi0001@users.noreply.github.com>
2025-11-30 16:52:30 -0300

    Update App.js

--- jogoclaudiaaaaaa/src/App.js
+++ jogoclaudiaaaaaa/src/App.js
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-import RouletteWheel from './RouletteWheel';
+import React from 'react';
 import SlotMachine from './SlotMachine';
 import './App.css';
 
 function App() {
-  const [currentGame, setCurrentGame] = useState('roulette');
-
   return (
     <div className="App">
       <h1 className="header-text">A Casa Sempre Ganha</h1>
       
-      <div className="game-selector">
-        <button 
-          onClick={() => setCurrentGame('roulette')}
-          className={currentGame === 'roulette' ? 'active' : ''}
-        >
-          Roleta
-        </button>
-        <button 
-          onClick={() => setCurrentGame('slots')}
-          className={currentGame === 'slots' ? 'active' : ''}
-        >
-          Caça-Níqueis
-        </button>
-      </div>
-
-      {currentGame === 'roulette' ? <RouletteWheel /> : <SlotMachine />}
+      <SlotMachine />
       
       <div className="footer">
-        <h3>Eu odeio github</h3>
+        <h3>eu odeio github</h3>
       </div>
     </div>
   );
